9c9194210ee651cc17cacfc770659431aafa7efc
anshumanojha <55602333+anshumanojha@users.noreply.github.com>
2023-07-28 14:44:11 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,6 +6,7 @@ import streamlit as st
 
 import streamlit as st
 import pandas as pd
+import plotly.express as px
 
 # Add Personal Information
 st.title("Anshuman's Portfolio")
@@ -66,6 +67,8 @@ st.markdown(
     "</ul>",
     unsafe_allow_html=True
 )
+
+
 
 
 
