f1a2057f5da694f286aae1b6e4de2f4b331a0e5f
anshumanojha <55602333+anshumanojha@users.noreply.github.com>
2023-07-28 16:20:24 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -74,9 +74,46 @@ st.markdown(
 st.title('Projects and Codes')
 st.subheader('Project 1: Data Scraping')
 st.write("Description: A Python script to scrape data from a website.")
-st.code('''
-# Your code for Data Scraping project goes here
-''', language='python')
+st.code(
+import requests
+import bs4
+import pandas as pd
+
+def get_smaller_urls(search_queries):
+    smaller_urls = []
+
+    for query in search_queries:
+        
+        url = "https://google.com/search?q=" + query
+
+        
+        request_result = requests.get(url)
+
+       
+        soup = bs4.BeautifulSoup(request_result.text, "html.parser")
+
+       
+        search_results = soup.find_all("a")
+        result_links = []
+
+        for result in search_results:
+            link = result.get("href")
+            if link.startswith("/url?q="):
+                smaller_url = link[7:].split("&sa")[0]
+                result_links.append(smaller_url)
+
+        smaller_urls.extend(result_links)
+
+    return smaller_urls
+
+
+search_queries = ["briyani in bangalore"]
+results = get_smaller_urls(search_queries)
+
+df = pd.DataFrame({"Search Query": [query for query in search_queries for _ in range(len(results))],
+                   "Smaller URL": results})
+print(df)
+, language='python')
 
 st.subheader('Project 2: Python Automation')
 st.write("Description: A Python script for automating a repetitive task.")
