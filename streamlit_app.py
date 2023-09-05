b017e29f3d1e1c4acb24e04c9783aef4a8467728
anshumanojha <55602333+anshumanojha@users.noreply.github.com>
2023-09-05 12:09:31 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,29 +1,45 @@
+import pandas as pd
 import streamlit as st
-import qrcode
 
-# Streamlit app title
-st.title("QR Code Generator")
 
-# Input field for the user to enter the URL
-user_url = st.text_input("Enter the URL:")
+import streamlit as st
+import plotly.graph_objects as go
 
-# Function to generate and display the QR code
-def generate_qr_code(url):
-    if url:
-        # Create the QR code
-        qr = qrcode.QRCode(
-            version=1,
-            error_correction=qrcode.constants.ERROR_CORRECT_L,
-            box_size=10,
-            border=4,
-        )
-        qr.add_data(url)
-        qr.make(fit=True)
-        qr_img = qr.make_image(fill_color="black", back_color="white")
+# Create bar chart for tools data
+tools_data = [10, 10, 8, 9, 7]
+tools_labels = ['MYSQL', 'Python', 'Dashboard Development', 'Power Bi']
+tools_fig = go.Figure(go.Bar(x=tools_labels, y=tools_data))
+tools_fig.update_layout(title='Tools Known')
+tools_chart = dict(zip(tools_labels, tools_data))
 
-        # Display the QR code
-        st.image(qr_img, use_column_width=True, caption="QR Code")
+# Create line chart for technology data
+technology_data = [9, 10, 10, 8, 7, 10]
+technology_labels = ['Superset', 'SQL', 'Python', 'AWS', 'AI', 'ML']
+technology_fig = go.Figure(go.Scatter(x=technology_labels, y=technology_data, mode='lines+markers'))
+technology_fig.update_layout(title='Technology Known')
+technology_chart = dict(zip(technology_labels, technology_data))
 
-# Button to trigger QR code generation
-if st.button("Generate QR Code"):
-    generate_qr_code(user_url)
+# Create pie chart for skills data
+skills_data = [30, 60, 25, 33]
+skills_labels = ['MYSQL', 'Python', 'Dashboard Development', 'Power Bi']
+skills_fig = go.Figure(go.Pie(labels=skills_labels, values=skills_data, hole=0.3))
+skills_fig.update_layout(title='Skills Proficiency out of 10')
+skills_chart = dict(zip(skills_labels, skills_data))
+
+# Define the layout of the dashboard
+st.title('Dashboard')
+
+st.header('Tools Known')
+st.plotly_chart(tools_fig)
+st.bar_chart(tools_chart)
+
+st.header('Technology Known')
+st.plotly_chart(technology_fig)
+st.line_chart(technology_chart)
+
+st.header('Skills Proficiency')
+st.plotly_chart(skills_fig)
+st.pyplot(skills_chart)
+
+st.header('Certifications')
+st.markdown('[IBM-Data Analysis certificate](https://www.coursera.org/account/accomplishments/certificate/PHKLT6LDUU3V)')
